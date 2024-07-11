c0c3bf89da20cd6d2ba91ff718cbf18dc9025a6b
Jason Sung <Jason.Sung@xuenn.com>
2024-07-11 19:11:47 +0800

    123

--- T9.py
+++ T9.py
@@ -4,7 +4,7 @@ import websockets
 import base64
 import json
 import os
-
+import sqlite3
 
 async def LoginGetToken():
     url = 'https://g.t9cn818.online/api/Lobby/login'
@@ -27,62 +27,73 @@ async def LoginGetToken():
     print(response.json())
     return response.json()
 
-async def EnterTable(websocket , login_data):
-        EnterTable_data = {
-            "OpCode": "EnterTable",
-            "Data": {
-                "GameType": "80001",
-                "TableId": "99",
-                "EnterTableInLobby": "true"
-            },
-            "Token": login_data['Data']['Token']
-            }
-        await websocket.send(json.dumps(EnterTable_data))
-    
+async def EnterTable(websocket, login_data):
+    EnterTable_data = {
+        "OpCode": "EnterTable",
+        "Data": {
+            "GameType": "80001",
+            "TableId": "99",
+            "EnterTableInLobby": "true"
+        },
+        "Token": login_data['Data']['Token']
+    }
+    await websocket.send(json.dumps(EnterTable_data))
 
 async def connect():
-    try:
-        login_data = await LoginGetToken()
-        websocketURL = login_data['Data']['ConnectIds'][0]
-        url = f'wss://g.t9cn818.online/api/baccarat{websocketURL}'
-        print(url)
+    while True:  # Outer loop for reconnecting
+        try:
+            login_data = await LoginGetToken()
+            websocketURL = login_data['Data']['ConnectIds'][0]
+            url = f'wss://g.t9cn818.online/api/baccarat{websocketURL}'
+            print(url)
 
-        sec_websocket_key = base64.b64encode(os.urandom(16)).decode('utf-8')
-        headers = {
-            'Origin': 'https://g.t9cn818.online',
-            'Host' : 'g.t9cn818.online'
-        }
+            headers = {
+                'Origin': 'https://g.t9cn818.online',
+                'Host': 'g.t9cn818.online'
+            }
 
-        async with websockets.connect(url, extra_headers=headers) as websocket:
-            if websocket :
-                auth_data1 = {
-                    "OpCode": "LoginGame",
-                    "Data": {
-                        "AgentId": "17619",
-                        "MemberName": "km831yt",
-                        "AccountType": "1",
-                        "Password": "ken456xyz1",  
-                        "GameType": "80001",
-                        "GetBroadCast": "1"
-                    },
-                    "Token": login_data['Data']['Token']
-                }
-                await websocket.send(json.dumps(auth_data1))
-                print(auth_data1)
-                
-                
-                while True:
-                    message = await websocket.recv()
-                    try:
-                        message_data = json.loads(message)
-                        if message_data['OpCode'] == 'LoginGame' :
-                            print("Enter Table Message")
-                            await EnterTable(websocket=websocket,login_data=login_data)
-                        print(f"Received JSON message: {message_data}")
-                    except json.JSONDecodeError:
-                        print(f"Received non-JSON message: {message}")
+            async with websockets.connect(url, extra_headers=headers) as websocket:
+                if websocket:
+                    auth_data = {
+                        "OpCode": "LoginGame",
+                        "Data": {
+                            "AgentId": "17619",
+                            "MemberName": "km831yt",
+                            "AccountType": "1",
+                            "Password": "ken456xyz1",
+                            "GameType": "80001",
+                            "GetBroadCast": "1"
+                        },
+                        "Token": login_data['Data']['Token']
+                    }
+                    await websocket.send(json.dumps(auth_data))
+                    print(auth_data)
 
-    except Exception as e:
-        print(f"Error: {str(e)}")
+                    while True:  # Inner loop for receiving messages
+                        message = await websocket.recv()
+                        try:
+                            message_data = json.loads(message)
+                            if message_data['OpCode'] == 'DisConnected':
+                                print("WebSocket disconnected.")
+                                break  # Exit the inner loop and reconnect
+
+                            elif message_data['OpCode'] == 'LoginGame':
+                                print("Enter Table Message")
+                                await EnterTable(websocket=websocket, login_data=login_data)
+                            elif message_data['OpCode'] == 'RoundResult':
+                                print(message_data)
+                            elif message_data['OpCode'] == 'StartGame':
+                                print(message_data)
+                            elif message_data['OpCode'] == 'Shuffle':
+                                print(message_data)
+                        except :
+                            print("Receive message error")
+
+        except websockets.ConnectionClosed:
+            print("WebSocket connection closed, retrying immediately...")
+
+        except Exception as e:
+            print(f"Error: {str(e)}")
+            print("Retrying immediately...")
 
 asyncio.get_event_loop().run_until_complete(connect())
--- test.py
+++ test.py
@@ -1,4 +1,1 @@
-import datetime
-
-expiration_time = datetime.datetime.fromtimestamp(1720685154)
-print("Token expiration time:", expiration_time)
+import sqlite3
