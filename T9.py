6635e1d1581c21edde2c509e41b921c79655b082
Jason Sung <Jason.Sung@xuenn.com>
2024-07-11 19:11:10 +0800

    New1

---
+++ T9.py
@@ -0,0 +1,88 @@
+import requests
+import asyncio
+import websockets
+import base64
+import json
+import os
+
+
+async def LoginGetToken():
+    url = 'https://g.t9cn818.online/api/Lobby/login'
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'content-type': 'application/json; charset=UTF-8',
+        'origin': 'https://g.t9cn818.online',
+    }
+    data = {
+        "Language": "TW",
+        "Account": "km831yt",
+        "Password": "ken456xyz1",
+        "SerialNumber": "qhqEgHivcW",
+        "Merchant": "_T9",
+        "Device": "2"
+    }
+    response = requests.post(url, headers=headers, json=data)
+    print(response.status_code)
+    print(response.json())
+    return response.json()
+
+async def EnterTable(websocket , login_data):
+        EnterTable_data = {
+            "OpCode": "EnterTable",
+            "Data": {
+                "GameType": "80001",
+                "TableId": "99",
+                "EnterTableInLobby": "true"
+            },
+            "Token": login_data['Data']['Token']
+            }
+        await websocket.send(json.dumps(EnterTable_data))
+    
+
+async def connect():
+    try:
+        login_data = await LoginGetToken()
+        websocketURL = login_data['Data']['ConnectIds'][0]
+        url = f'wss://g.t9cn818.online/api/baccarat{websocketURL}'
+        print(url)
+
+        sec_websocket_key = base64.b64encode(os.urandom(16)).decode('utf-8')
+        headers = {
+            'Origin': 'https://g.t9cn818.online',
+            'Host' : 'g.t9cn818.online'
+        }
+
+        async with websockets.connect(url, extra_headers=headers) as websocket:
+            if websocket :
+                auth_data1 = {
+                    "OpCode": "LoginGame",
+                    "Data": {
+                        "AgentId": "17619",
+                        "MemberName": "km831yt",
+                        "AccountType": "1",
+                        "Password": "ken456xyz1",  
+                        "GameType": "80001",
+                        "GetBroadCast": "1"
+                    },
+                    "Token": login_data['Data']['Token']
+                }
+                await websocket.send(json.dumps(auth_data1))
+                print(auth_data1)
+                
+                
+                while True:
+                    message = await websocket.recv()
+                    try:
+                        message_data = json.loads(message)
+                        if message_data['OpCode'] == 'LoginGame' :
+                            print("Enter Table Message")
+                            await EnterTable(websocket=websocket,login_data=login_data)
+                        print(f"Received JSON message: {message_data}")
+                    except json.JSONDecodeError:
+                        print(f"Received non-JSON message: {message}")
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+asyncio.get_event_loop().run_until_complete(connect())
---
+++ test.py
@@ -0,0 +1,4 @@
+import datetime
+
+expiration_time = datetime.datetime.fromtimestamp(1720685154)
+print("Token expiration time:", expiration_time)
