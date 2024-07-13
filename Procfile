a16c3769a3621369635111f5035e5d8a77d63132
Jason Sung <jason870724@gmail.com>
2024-07-13 20:49:20 +0800

    procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python T9.py
--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-import sqlite3
+import magic
+
+file_path = 'T9.db'
+mime = magic.Magic(mime=True)
+file_type = mime.from_file(file_path)
+print(f'MIME type: {file_type}')
