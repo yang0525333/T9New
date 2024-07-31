484313a775101ffc50901d18924036896991b00f
Jason Sung <Jason.Sung@xuenn.com>
2024-07-31 17:22:26 +0800

    pair

--- T9.py
+++ T9.py
@@ -392,11 +392,11 @@ async def CheckProbability():
         LuckySixProbability = (TotalLuckySix / TotleGameRound) * 100
         PlayerpairProbability = (TotalPlayerpair / TotleGameRound) * 100
         BankerpairProbability = (TotalBankerpair / TotleGameRound) * 100
-        if PlayerProbability < 41 :
-            message = f'近一小時內每桌總和後"閒家"勝率僅剩{round(PlayerProbability, 2)}% , https://t9live-b5c2cbf5b1b9.herokuapp.com/'
+        if PlayerpairProbability < 41 :
+            message = f'近5分鐘內每桌總和後"閒對"勝率僅剩{round(PlayerpairProbability, 2)}% , https://t9live-b5c2cbf5b1b9.herokuapp.com/'
             await LineNotify(message)
-        if BankerProbability < 41 :
-            message = f'近一小時內每桌總和後"莊家"家勝率僅剩{round(BankerProbability, 2)}% , https://t9live-b5c2cbf5b1b9.herokuapp.com/'
+        if BankerpairProbability < 41 :
+            message = f'近5分鐘內每桌總和後"莊對"家勝率僅剩{round(BankerpairProbability, 2)}% , https://t9live-b5c2cbf5b1b9.herokuapp.com/'
             await LineNotify(message)
         conn = await get_db_connection()
         cursor = conn.cursor()
